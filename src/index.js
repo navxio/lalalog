53e6813c7941a2ecd21ef9ef62c9a52ffbac0a6d
Nav Saini <navdeep@mailbox.org>
2021-03-04 00:34:05 +0530

    second commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+/*
+ * lalalog
+ * created by Nav Saini @navxio
+ * on Wed, March 3, 2021
+ */
+
+const log = {};
+
+// the statics are
+
+// primary
+// secondary
+// success
+// danger
+// warning
+// info
+// light
+// dark
+
+const styleString = '%c ';
+log.static = (args) => {
+	console.log.apply();
+};
+
+export default log;
